34ac89e9daeb2d6657b012e72479fa0cc9b7edc2
Muhamad Chaerul Anwar <96242857+anwar1622@users.noreply.github.com>
2022-10-24 21:15:54 +0700

    Update

--- database/database.go
+++ database/database.go
@@ -3,34 +3,40 @@ package database
 import (
 	"App/models"
 	"fmt"
+	"log"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"log"
 )
 
-type Config struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	DBName   string
-	SSLMode  string
-}
+// type Config struct {
+// 	Host     string
+// 	Port     string
+// 	User     string
+// 	Password string
+// 	DBName   string
+// 	SSLMode  string
+// }
 
 var (
-	db  *gorm.DB
-	err error
+	host     = os.Getenv("PGHOST")
+	user     = os.Getenv("PGUSER")
+	password = os.Getenv("PGPASSWORD")
+	dbPort   = os.Getenv("PGPORT")
+	dbName   = os.Getenv("PGDATABASE")
+	db       *gorm.DB
+	err      error
 )
 
-func StartDB(config *Config) (*gorm.DB, error) {
-	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", config.Host, config.User, config.Password, config.DBName, config.Port, config.SSLMode)
+func StartDB() {
+	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbName, dbPort)
 	db, err = gorm.Open(postgres.Open(conn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database", err)
 	}
 
 	db.AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
-	return db, err
 }
 
 func GetDB() *gorm.DB {
